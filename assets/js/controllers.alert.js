7de87332f1926f8df315fa2f13f77c6338ff3cc9
Antoine Moser <moser.antoine@gmail.com>
2015-02-27 23:37:22 +0100

    style: re-indent file with new js-beautify configuration

--- assets/js/controllers.alert.js
+++ assets/js/controllers.alert.js
@@ -1,64 +1,64 @@
 function AlertListController($scope, $location, $routeParams, AlertsGroup, sharedProperties) {
 
-    $scope.groupId = $routeParams.groupId;
+  $scope.groupId = $routeParams.groupId;
 
-    $scope.refreshAlertsGroup = function() {
-        AlertsGroup.get({
-            id: $scope.groupId
-        }, function(alertsGroup) {
-            $scope.alertsGroup = alertsGroup;
-        });
-    }
-
-    function isInThisGroup(element) {
-        return element.alertsGroupId == $scope.groupId;
-    }
-
-    $scope.alerts = [];
-
-    var currentLocationRoot = $location.protocol() + '://' + $location.host() + ':' + $location.port();
-    console.info('Websocket connection url : ' + currentLocationRoot);
-    var socket = io.connect(currentLocationRoot);
-
-    socket.on('alert_list_changed', function(newAlertList) {
-        console.info("Updating alert list...");
-        var filtered = newAlertList.filter(isInThisGroup);
-        $scope.$apply(function() {
-            $scope.alerts = filtered;
-        });
-        $scope.refreshAlertsGroup();
+  $scope.refreshAlertsGroup = function() {
+    AlertsGroup.get({
+      id: $scope.groupId
+    }, function(alertsGroup) {
+      $scope.alertsGroup = alertsGroup;
     });
+  }
 
-    $scope.$on('$routeChangeSuccess', function() {
-        socket.emit('refresh', {});
-        console.info('Refresh data...');
+  function isInThisGroup(element) {
+    return element.alertsGroupId == $scope.groupId;
+  }
+
+  $scope.alerts = [];
+
+  var currentLocationRoot = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+  console.info('Websocket connection url : ' + currentLocationRoot);
+  var socket = io.connect(currentLocationRoot);
+
+  socket.on('alert_list_changed', function(newAlertList) {
+    console.info("Updating alert list...");
+    var filtered = newAlertList.filter(isInThisGroup);
+    $scope.$apply(function() {
+      $scope.alerts = filtered;
     });
+    $scope.refreshAlertsGroup();
+  });
 
-    $scope.isAssigned = function() {
-        return $scope.alertsGroup != undefined && $scope.alertsGroup.assigned != undefined;
-    }
-    $scope.isOpened = function() {
-        return $scope.alertsGroup != undefined && $scope.alertsGroup.state == 'OPEN';
-    }
+  $scope.$on('$routeChangeSuccess', function() {
+    socket.emit('refresh', {});
+    console.info('Refresh data...');
+  });
+
+  $scope.isAssigned = function() {
+    return $scope.alertsGroup != undefined && $scope.alertsGroup.assigned != undefined;
+  }
+  $scope.isOpened = function() {
+    return $scope.alertsGroup != undefined && $scope.alertsGroup.state == 'OPEN';
+  }
 
 
-    // default sort
-    $scope.alertSortPredicate = 'date';
-    $scope.reverse = true;
+  // default sort
+  $scope.alertSortPredicate = 'date';
+  $scope.reverse = true;
 
-    $scope.assignTo = function() {
-        var assigned = sharedProperties.getCurrentUser().username;
-        console.debug("The alert will be assigned to : " + assigned);
-        $scope.alertsGroup.assigned = assigned;
-        AlertsGroup.update({
-            id: $scope.entryId
-        }, $scope.alertsGroup, function(response) {});
-    }
+  $scope.assignTo = function() {
+    var assigned = sharedProperties.getCurrentUser().username;
+    console.debug("The alert will be assigned to : " + assigned);
+    $scope.alertsGroup.assigned = assigned;
+    AlertsGroup.update({
+      id: $scope.entryId
+    }, $scope.alertsGroup, function(response) {});
+  }
 
-    $scope.closeAlertsGroup = function() {
-        $scope.alertsGroup.state = 'CLOSED';
-        AlertsGroup.update({
-            id: $scope.entryId
-        }, $scope.alertsGroup, function(response) {});
-    }
+  $scope.closeAlertsGroup = function() {
+    $scope.alertsGroup.state = 'CLOSED';
+    AlertsGroup.update({
+      id: $scope.entryId
+    }, $scope.alertsGroup, function(response) {});
+  }
 };
--- assets/js/services.js
+++ assets/js/services.js
@@ -1,13 +1,13 @@
 // define rest services
 
 angular.module('alertsGroupService', ['ngResource']).factory('AlertsGroup', function($resource) {
-    return $resource('alertsGroup/:id', {}, {
-        update: {
-            method: 'PUT'
-        }
-    });
+  return $resource('alertsGroup/:id', {}, {
+    update: {
+      method: 'PUT'
+    }
+  });
 });
 
 angular.module('actionService', ['ngResource']).factory('Action', function($resource) {
-    return $resource('action/:id', {}, {});
+  return $resource('action/:id', {}, {});
 });
